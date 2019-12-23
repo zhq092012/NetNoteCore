66ff6f004d59089e059fec8d72dff5f4717889e8
zhq092012 <zhq_092012@163.com>
2019-12-23 15:28:52 +0800

    1111

---
+++ NetNote.Models/NoteContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetNote.Models
+{
+  public class NoteContextFactory : IDesignTimeDbContextFactory<NoteContext>
+  {
+    public NoteContext CreateDbContext(string[] args)
+    {
+      var optionsBuilder = new DbContextOptionsBuilder<NoteContext>();
+      optionsBuilder.UseSqlServer("server=localhost;uid=sa;pwd=123456;database=NoteDb");
+
+      return new NoteContext(optionsBuilder.Options);
+    }
+  }
+}
