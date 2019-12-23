eae6747c40a935d00f6cefe780260b1a75231717
zhq092012 <zhq_092012@163.com>
2019-12-23 16:13:39 +0800

    1111

--- NetNote/Startup.cs
+++ NetNote/Startup.cs
@@ -9,12 +9,33 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;using NetNote.IService;using NetNote.Models;using NetNote.Service;namespace NetNote
-{  public class Startup  {    public Startup(IConfiguration configuration)    {      Configuration = configuration;    }    public IConfiguration Configuration { get; }    // This method gets called by the runtime. Use this method to add services to the container.    public void ConfigureServices(IServiceCollection services)    {
-      services.AddDbContext<NoteContext>(options => 
-                          options.UseMySql(Configuration.GetConnectionString("NoteDb"),
-                          b=>b.MigrationsAssembly("NetNote"))//将迁移上下文合并到当前程序集s
-                          .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));//所有查询都在 DbContext 的无跟踪中执行
+{  public class Startup  {    public Startup(IConfiguration configuration)    {      Configuration = configuration;    }    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)    {
+      var dbtype = Configuration.GetSection("DbType").Value;
+      if (dbtype=="SqlServer")
+      {
+        services.AddDbContext<NoteContext>(options =>
+                       options.UseSqlServer(Configuration.GetConnectionString("SqlServer"),
+                       b => b.MigrationsAssembly("NetNote"))//将迁移上下文合并到当前程序集s
+                       .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));//所有查询都在 DbContext 的无跟踪中执行
+      }
+      if (dbtype=="Mysql")
+      {
+        services.AddDbContext<NoteContext>(options =>
+                        options.UseMySql(Configuration.GetConnectionString("Mysql"),
+                        b => b.MigrationsAssembly("NetNote"))//将迁移上下文合并到当前程序集s
+                        .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));//所有查询都在 DbContext 的无跟踪中执行
+      }
+    
+   
       services.AddScoped<INoteRepository, NoteRepository>();
       services.AddScoped<INoteTypeRepository, NoteTypeRepository>();
-      services.Configure<CookiePolicyOptions>(options =>      {              // This lambda determines whether user consent for non-essential cookies is needed for a given request.              options.CheckConsentNeeded = context => true;        options.MinimumSameSitePolicy = SameSiteMode.None;      });      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);    }    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.    public void Configure(IApplicationBuilder app, IHostingEnvironment env)    {      if (env.IsDevelopment())      {        app.UseDeveloperExceptionPage();      }      else      {        app.UseExceptionHandler("/Home/Error");        app.UseHsts();      }      app.UseHttpsRedirection();      app.UseStaticFiles();      app.UseCookiePolicy();      app.UseMvc(routes =>      {        routes.MapRoute(                  name: "default",                  template: "{controller=Home}/{action=Index}/{id?}");      });    }  }
+      services.Configure<CookiePolicyOptions>(options =>      {
+        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+        options.CheckConsentNeeded = context => true;        options.MinimumSameSitePolicy = SameSiteMode.None;      });      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)    {      if (env.IsDevelopment())      {        app.UseDeveloperExceptionPage();      }      else      {        app.UseExceptionHandler("/Home/Error");        app.UseHsts();      }      app.UseHttpsRedirection();      app.UseStaticFiles();      app.UseCookiePolicy();      app.UseMvc(routes =>      {        routes.MapRoute(                  name: "default",                  template: "{controller=Home}/{action=Index}/{id?}");      });    }  }
 }
